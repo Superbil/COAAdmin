ed878a03bf608633c8e51499c9e484c2f84f8348
Superbil <me@superbil.co>
2020-12-24 04:09:37 +0800

    Coding style

--- public/js/custom.js
+++ public/js/custom.js
@@ -22,7 +22,7 @@ function rwd_fun() {
     } else {
         $('#rwd_nav').before($('#header_logo'));
     }
-}
+};
 
 $(document).ready(function(){
     $('#m_nav a:lt(3)').click(function(){
@@ -40,86 +40,87 @@ $(document).ready(function(){
             location.href="/locales/zhtw";
         }
     });
-})
+});
 
 function renderAtVOWL(json) {
-  var height = 600, width = 1000;
-  var graphElement = document.getElementById('graph');
+    var height = 600, width = 1000;
+    var graphElement = document.getElementById('graph');
 
-  var config = {
-    "key1": "Product",
-    "key2": "Operation",
-    "key3": "X",
-    "label1": "Product_label",
-    "label2": "Operation_label",
-  }
-  var graph = sparql2graph(json, config);
+    var config = {
+        "key1": "Product",
+        "key2": "Operation",
+        "key3": "X",
+        "label1": "Product_label",
+        "label2": "Operation_label",
+    }
+    var graph = sparql2graph(json, config);
 
-  console.log('renderAtVOWL');
-  console.dir(graph);
-  if (graph === undefined) { return; }
-  drawGraph(graphElement, width, height, graph);
-}
+    console.log('renderAtVOWL');
+    console.dir(graph);
+    if (graph === undefined) { return; }
+
+    drawGraph(graphElement, width, height, graph);
+};
 
 function sparql2graph(json, config) {
-  if (json === undefined || json.results.bindings === undefined) {
-    console.log('no json.results.bindings');
-    return;
-  }
-
-  var data = json.results.bindings;
-  var graph = {
-    "nodes": [],
-    "links": []
-  }
-  var check = d3.map();
-  var index = 0;
-  var hasColorFirst = false;
-
-  for (var i = 0; i < data.length; i++) {
-    var key1Value = data[i][config.key1].value
-    var key2Value = data[i][config.key2].value
-    var key3Value = data[i][config.key3].value
-    var label1 = config.label1 ? data[i][config.label1].value : key1Value
-    var label2 = config.label2 ? data[i][config.label2].value : key2Value
-
-    if (!check.has(key1Value)) {
-      graph.nodes.push({
-        "key": key1Value,
-        "name": label1,
-        "group": key3Value,
-        "type": hasColorFirst ? 'rdfsClass' : 'class'
-      });
-      check.set(key1Value, index);
-      index++;
+    if (json === undefined || json.results.bindings === undefined) {
+        console.log('no json.results.bindings');
+        return;
     }
 
-    if (!check.has(key2Value)) {
-      graph.nodes.push({
-        "key": key2Value,
-        "name": label2,
-        "group": key3Value + 1,
-        "type": hasColorFirst ? 'rdfsClass' : 'class'
-      });
-      hasColorFirst = true;
-      check.set(key2Value, index);
-      index++;
+    var data = json.results.bindings;
+    var graph = {
+        "nodes": [],
+        "links": []
+    }
+    var check = d3.map();
+    var index = 0;
+    var hasColorFirst = false;
+
+    for (var i = 0; i < data.length; i++) {
+        var key1Value = data[i][config.key1].value
+        var key2Value = data[i][config.key2].value
+        var key3Value = data[i][config.key3].value
+        var label1 = config.label1 ? data[i][config.label1].value : key1Value
+        var label2 = config.label2 ? data[i][config.label2].value : key2Value
+
+        if (!check.has(key1Value)) {
+            graph.nodes.push({
+                "key": key1Value,
+                "name": label1,
+                "group": key3Value,
+                "type": hasColorFirst ? 'rdfsClass' : 'class'
+            });
+            check.set(key1Value, index);
+            index++;
+        }
+
+        if (!check.has(key2Value)) {
+            graph.nodes.push({
+                "key": key2Value,
+                "name": label2,
+                "group": key3Value + 1,
+                "type": hasColorFirst ? 'rdfsClass' : 'class'
+            });
+            hasColorFirst = true;
+            check.set(key2Value, index);
+            index++;
+        }
+
+        if (check.has(key1Value) && check.has(key2Value)) {
+            var key1ValueAndKey2Value = key1Value + '_' + key2Value;
+            if (!check.has(key1ValueAndKey2Value)) {
+                graph.links.push({
+                    "source": check.get(key1Value),
+                    "target": check.get(key2Value)
+                });
+                check.set(key1ValueAndKey2Value);
+            }
+        }
     }
 
-    if (check.has(key1Value) && check.has(key2Value)) {
-      var key1ValueAndKey2Value = key1Value + '_' + key2Value;
-      if (!check.has(key1ValueAndKey2Value)) {
-        graph.links.push({
-          "source": check.get(key1Value),
-          "target": check.get(key2Value)
-        });
-        check.set(key1ValueAndKey2Value);
-      }
-    }
-  }
-
-  return graph;
-}
+    return graph;
+};
 
 // Local Variables:
 // js-indent-level: 4
